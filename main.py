fb581f528576af21ac0ca000c2db899e6dcd97be
MDalamin5 <mdal.amin5@northsouth.edu>
2025-05-16 00:07:10 +0600

    Patient management system by FastAPI

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+        
+        return data
 
 @app.get("/")
 def home():
     return {
-        "messages": "Assalamualikm sir."
+        "messages": "Patient Management System API"
     }
     
 @app.get("/about")
 def about():
     return {
-        "messages": "This is test about page"
+        "messages": "A fully function API to manage your patient records."
     }
+    
+    
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
     
