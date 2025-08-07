ca120dbb980c4b42a5047136ed506e380557ab7d
MDalamin5 <mdal.amin5@northsouth.edu>
2025-08-07 22:59:33 +0600

    fetch data from database

--- 04-FastAPI-with-Bitfumes/blog/main.py
+++ 04-FastAPI-with-Bitfumes/blog/main.py
@@ -34,3 +34,14 @@ def create(request: Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get("/blog")
+def all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    
+    return blogs
+
+@app.get("/blog/{id}")
+def show(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
