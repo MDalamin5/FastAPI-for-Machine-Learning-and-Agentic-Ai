c51c53df4ba5b62a538d315cb5c566f8c731a49d
MDalamin5 <mdal.amin5@northsouth.edu>
2025-08-07 15:22:23 +0600

    fast api basics end point creating

--- 04-FastAPI-with-Bitfumes/main.py
+++ 04-FastAPI-with-Bitfumes/main.py
@@ -5,6 +5,7 @@ app = FastAPI(
     description="Build CRUD api to interact with database."
 )
 
+##
 
 @app.get("/")
 def home():
@@ -15,6 +16,36 @@ def home():
             "Role": "Ai Engineer"
         }
     }
+    
+@app.get("/blog/unpublish")
+def unpublish():
+    return {
+        "data": "List of unpublish blog"
+    }
+    
+    
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch the blog based on the id
+    return {
+        "data": id
+    }
+
+    
+
+    
+@app.get("/blog/{id}/comments")
+def comments(id):
+    ## fetch the comment of the blog with id = id
+    
+    return {
+        "data": {
+            'cmnt_1': "content...",
+            'cmnt_2': "content...",
+            'cmnt_3': "content...",
+        }
+    }
+    
     
 @app.get("/about")
 def about():
