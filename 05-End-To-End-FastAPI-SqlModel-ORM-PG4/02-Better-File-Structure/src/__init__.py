e820650e8a9e2d259f6e836c39118b33b4a300e9
mdalamin5 <mdal.amin5@northsouth.edu>
2025-09-09 16:22:56 +0600

    through sqlmodel create the table into the postgres db

--- 05-End-To-End-FastAPI-SqlModel-ORM-PG4/02-Better-File-Structure/src/__init__.py
+++ 05-End-To-End-FastAPI-SqlModel-ORM-PG4/02-Better-File-Structure/src/__init__.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print("Server is Started......")
+    await init_db()
+    yield
+
+    print("Server is stopped")
 
 version = "v1"
 
 app = FastAPI(
     title="Advanced Api Building Using Router Process.",
-    version=version
+    version=version,
+    lifespan = life_span,
+    description="Design REST API for book review web Service."
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"]) # this prefix will be add every router as link: "/books/{other value in the define in router app}"
---
+++ 05-End-To-End-FastAPI-SqlModel-ORM-PG4/02-Better-File-Structure/src/config.py
@@ -0,0 +1,32 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+# Load the .env file
+load_dotenv()
+
+class Settings(BaseSettings):
+    """Manages Postgres database settings."""
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        # Encode password to handle special characters
+        encoded_password = quote_plus(self.DB_PASSWORD)
+        return f"postgresql+asyncpg://{self.DB_USER}:{encoded_password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+# Instantiate settings
+settings = Settings()
+
+if __name__ == "__main__":
+    print(settings.DATABASE_URL)
