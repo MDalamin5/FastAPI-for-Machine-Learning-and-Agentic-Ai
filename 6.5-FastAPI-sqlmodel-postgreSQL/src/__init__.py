a40af848406c1d329d29b332849e9a71d044b3d8
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-16 20:11:13 +0600

    Revert "table migrations using alembic"
    This reverts commit cafc951e6e563a2587d66eee3f75062855b6a33c.

--- 6.5-FastAPI-sqlmodel-postgreSQL/src/__init__.py
+++ 6.5-FastAPI-sqlmodel-postgreSQL/src/__init__.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from src.db.main import init_db
 from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Server is starting...")
-    await init_db()
     yield 
     print("Server is shutting down...")
 
