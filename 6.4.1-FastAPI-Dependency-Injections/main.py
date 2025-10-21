e9fccd998d2e06a2f5983ca03cc6ddfbfa8167b2
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-21 16:33:10 +0600

    implement fastapi dependency functions

--- 6.4.1-FastAPI-Dependency-Injections/main.py
+++ 6.4.1-FastAPI-Dependency-Injections/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Depends
 from typing import Annotated
+from dataclasses import dataclass
 
 app = FastAPI()
 
 
+# -------<Dependency Functions>---------
 def provide_inputs(x: int , y: int, z: str):
     if x < 0:
         raise ValueError("X is not less then 0.")
@@ -15,6 +17,13 @@ def provide_inputs(x: int , y: int, z: str):
         "y": y,
         "zz": z
     }
+
+# -------------<Dependency Class>-----------
+
+@dataclass
+class Input:
+    x: int
+    y: int
 
 @app.get("/")
 async def home(inputs: Annotated[dict, Depends(provide_inputs)]):
@@ -32,4 +41,12 @@ async def testline(inputs: Annotated[dict, Depends(provide_inputs)]):
     return {
         "square_x": f"{inputs.get("x") ** 2}",
         "square_y": f"{inputs.get("y") ** 2}"
+    }
+
+@app.get("/class_call")
+async def class_val(inputs: Annotated[Input, Depends(Input)]):
+
+    return {
+        "square_x": f"{inputs.x ** 2}",
+        "square_y": f"{inputs.y ** 2}"
     }
