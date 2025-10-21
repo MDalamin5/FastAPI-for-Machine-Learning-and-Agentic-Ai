4b8d182bf339354d7e685285b011f7d77b31bdbe
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-21 18:01:15 +0600

    start oop with self keyword

---
+++ 0.5-OOP-Python/00.py
@@ -0,0 +1,19 @@
+## Create own class
+
+class Animal:
+    def __init__(alam, data):
+        alam.x = data
+
+
+    def test(tst):
+        print("hi...", tst.x)
+
+    def bark(alamin):
+        print("test line")
+        alamin.test()
+
+
+
+if __name__ == "__main__":
+    man = Animal(4)
+    man.bark()
