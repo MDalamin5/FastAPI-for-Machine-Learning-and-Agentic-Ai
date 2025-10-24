ddcfe9f1fef5dc4bb03b35474a88ca8c2d05195e
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-24 20:14:17 +0600

    unpacking *arg variable

--- 0.7-Advanced-Python-Features/feature_1_unpacking.py
+++ 0.7-Advanced-Python-Features/feature_1_unpacking.py
@@ -1,3 +1,12 @@
 # Basics Unpacking
 a, b, c = [1, 2, 3]
 print(f"a: {a}, b: {b}, c: {c}" )
+
+# extended Iterable Unpacking with *
+a, b, *c = [1, 2, 3, 4, 5, 6]
+print(f"a: {a}, b: {b}, c: {c}")
+print(type(a))
+print(type(c))
+
+# Ignoring Values by using '_'
+a, _, c = [1, 2, 3]
