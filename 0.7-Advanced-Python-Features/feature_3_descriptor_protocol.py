92ba8437ad53b183bac680f294678cef4eebbc84
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-26 22:31:06 +0600

    function description protocol

---
+++ 0.7-Advanced-Python-Features/feature_3_descriptor_protocol.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def __repr__(self) -> str:
+        """
+        __repr__ is meant to 
+        """
