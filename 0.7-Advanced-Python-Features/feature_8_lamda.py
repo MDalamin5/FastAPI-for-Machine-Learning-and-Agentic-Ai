11fae817a864619a4c09a43099a81f38e16de3d2
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-31 18:37:09 +0600

    lambda with map functions

--- 0.7-Advanced-Python-Features/feature_8_lamda.py
+++ 0.7-Advanced-Python-Features/feature_8_lamda.py
@@ -14,3 +14,26 @@ print(val(4,0))
 
 check = lambda i: i in "hello"
 print(check('h'))
+
+
+## Integrate lambda functions as map functions function parameter.
+
+# task: prices are stored as messy strings and need cleaning to float
+
+
+
+## remove '$' sign before the number
+## brainstorming
+p = "$32.33"
+val = p.replace("$", "")
+print(type(float(val)))
+
+## brainstorming end <---
+
+cleaning = lambda x: float(x.replace("$", ""))
+
+cleaning("$33.3")
+
+## now whole things apply on the list
+prices = ["$12.33", "$33.32", "$23.44"]
+list(map(lambda p: float(p.replace("$", "")), prices))
