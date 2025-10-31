bf4e4f06bf73853f15482f55f70f016d7e21e768
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-31 19:58:33 +0600

    lambda with map, filter sorted functions

--- 0.7-Advanced-Python-Features/feature_8_lamda.py
+++ 0.7-Advanced-Python-Features/feature_8_lamda.py
@@ -37,3 +37,28 @@ cleaning("$33.3")
 ## now whole things apply on the list
 prices = ["$12.33", "$33.32", "$23.44"]
 list(map(lambda p: float(p.replace("$", "")), prices))
+
+
+## Lambda with filter functions
+
+get_even = lambda x: x % 2 == 0
+
+print(get_even(4))
+
+num = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(list(filter((lambda x: x % 2 == 0), num)))
+
+## keep only student with names starting with 'M'
+
+students = [
+    ["Aminul", 23],
+    ["Alamin", 27],
+    ["Mohimen", 25]
+]
+
+
+list(filter((lambda row: row[0].startswith("A") if True else False), students))
+
+number = [32, 24, 5,6,2]
+sorted(number, reverse=True)
