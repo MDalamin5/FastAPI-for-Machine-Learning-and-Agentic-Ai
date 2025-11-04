8545b6167fb4ac77ff1a344a7e45aef20586ef91
mdalamin5 <mdal.amin5@northsouth.edu>
2025-11-04 12:17:15 +0600

    code management using modular coding

---
+++ 0.51-OOP-5h/atm-bank-Moduler-Coding/db.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        pass
+
+    def connecting_database(self):
+        print("Connecting to the database...")
---
+++ 0.51-OOP-5h/atm-bank-Moduler-Coding/main.py
@@ -0,0 +1,5 @@
+from service import AtmMachine
+from db import Database
+db = Database()
+db.connecting_database()
+dbbl = AtmMachine()
